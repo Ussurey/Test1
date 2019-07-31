aacb75c37d999d27e1908ca6f635c340c5c10203
Ussurey <ussurey@mail.ru>
2019-07-31 11:18:33 +0400

    Initial commit

---
+++ Test1.java
@@ -0,0 +1,19 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		System.out.println("Hello, World!");
+		System.out.println("Hello, World!");
+		System.out.println("Hi all!");
+		test2();
+		
+	}
+	
+	public static void test2() {
+		int x=100;
+		System.out.println(x);
+		System.out.println(x*10);
+	}
+}
