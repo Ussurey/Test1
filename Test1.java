57b67de3a82e642083022635c3f1e1dfae35f992
Ussurey <ussurey@mail.ru>
2019-08-01 15:08:44 +0400

    A

--- Test1.java
+++ Test1.java
@@ -5,9 +5,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
-		System.out.println("Hello, World!");
-		System.out.println("Hello, World!");
-		System.out.println("Hi all!");
 		test2();
 		
 		Class1 class1obj = new Class1("New object", 1);
